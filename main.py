cdb5533cacb2d59c8754e411761318fe9c8c8b0a
gradientBoom <ouzijie2020@163.com>
2025-08-01 12:13:53 -0400

    Achieve Fourier and inverse Fourier Transform. Tried do damaging in Fourier image

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 """
 Where our works begin
 """
+import numpy as np
+
+from L1_minimization import *
 from util import *
 
 if __name__ == "__main__"  :
-    pass
+    # read grayscale image
+    image = cv.imread("smallTriangle.png", cv.IMREAD_GRAYSCALE)
+
+    show(image)
+
+    result = do_Fourier_transform(image)
+
+    missing = np.complex128(0 + 1j)
+
+    print(result[0])
+
+    result[0][2] = missing
+
+    print(result[0])
+
+    result_hat = do_inverse_Fourier_transform(result)
+
+    show(result_hat)
+
+
 
 
 
